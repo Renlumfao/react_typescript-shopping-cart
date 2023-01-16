a66ee7f9e9b3365a5c53c0cb0f34ee2ce76a111a
MafiaDon2011 <wajidhussain1@live.com>
2023-01-16 15:21:17 +0000

    styled the navbar, and store body

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import Home from "./pages/Home"
+import { Store } from "./pages/Store"
+import About from "./pages/About"
+import { Navbar } from "./componets/Navbar"
 
 function App() {
- return <h1>hi</h1>
+ return (
+  <>
+  <Navbar />
+  <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Container>
+  </>
+ )
 }
 
 export default App
