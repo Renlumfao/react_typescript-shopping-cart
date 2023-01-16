fe2ebdef609db6d384417735495af577bab4c4b1
MafiaDon2011 <wajidhussain1@live.com>
2023-01-16 16:39:41 +0000

    completed the rest of the functions for shopping cart context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -12,7 +12,7 @@ type CartItem = {
 type ShoppingCartContext = {
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
-    descreaseCartQantity: (id: number) => void
+    decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
 }
 
@@ -44,9 +44,29 @@ export function ShoppingCartProvider( { children }: ShoppingCartProviderProps) {
             }
         })
     }
+    function decreaseCartQuantity(id: number) {
+        setCartItems(currItems => {
+          if (currItems.find(item => item.id === id)?.quantity === 1) {
+            return currItems.filter(item => item.id !== id)
+          } else {
+            return currItems.map(item => {
+              if (item.id === id) {
+                return { ...item, quantity: item.quantity - 1 }
+              } else {
+                return item
+              }
+            })
+          }
+        })
+      }
+      function removeFromCart(id: number) {
+        setCartItems(currItems => {
+          return currItems.filter(item => item.id !== id)
+        })
+      }
 
     return (
-        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity }}>
+        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
             {children}
         </ShoppingCartContext.Provider>
     )
